115fc490194fe11fcfbedc93afca4ecc9597c3e2
PC219-24\NTUT <kt40909.tyk@gamil.com>
2017-06-25 18:05:07 +0800

    新增物品修正閃退問題,缺檔案名稱

--- app/src/main/java/com/example/ntut/weshare/goods/GoodsInsertActivity.java
+++ app/src/main/java/com/example/ntut/weshare/goods/GoodsInsertActivity.java
@@ -8,6 +8,9 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
@@ -22,6 +25,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ntut.weshare.Common;
+import com.example.ntut.weshare.MainActivity;
 import com.example.ntut.weshare.R;
 
 import java.io.ByteArrayOutputStream;
@@ -37,14 +41,16 @@ public class GoodsInsertActivity extends AppCompatActivity {
     private EditText etQty;
     private EditText etComment;
     private Spinner spClass;
-    int intspClass;
+    private int intspClass;
     private Spinner spLoc;
     private int intspLoc;
+    private Spinner spDlvWay;
+    private int intspDlvWay;
     private ImageView ivImage;
     private int mYear,mMonth,mDay;
     private  byte[] image;
+    private String fileName;
     private File file;
-    private String indId="indId";
     Timestamp now= new Timestamp(System.currentTimeMillis());
     private static final int REQUEST_TAKE_PICTURE = 0;
     private static final int REQUEST_PICK_IMAGE = 1;
@@ -57,50 +63,25 @@ public class GoodsInsertActivity extends AppCompatActivity {
         spClass=(Spinner) findViewById(R.id.sp_class);
         final String[] classes = {"食","衣","住","行","育","樂"};
         ArrayAdapter<String> classList = new ArrayAdapter<>(GoodsInsertActivity.this,
-                android.R.layout.simple_spinner_dropdown_item,
-                classes);
+                android.R.layout.simple_spinner_dropdown_item, classes);
         spClass.setAdapter(classList);
-        switch (spClass.getSelectedItem().toString()){
-           case "食":
-              int intspClass=6;
-               break;
-            case "衣":
-                intspClass=1;
-                break;
-            case "住":
-                intspClass=2;
-                break;
-            case "行":
-                intspClass=3;
-                break;
-            case "育":
-                intspClass=4;
-                break;
-            case "樂":
-                intspClass=5;
-                break;
-        }
-
 
 
         spLoc=(Spinner) findViewById(R.id.sp_loc);
         final String[] loc = {"北","中","南"};
         ArrayAdapter<String> locList = new ArrayAdapter<>(GoodsInsertActivity.this,
-                android.R.layout.simple_spinner_dropdown_item,
-                loc);
+                android.R.layout.simple_spinner_dropdown_item, loc);
         spLoc.setAdapter(locList);
         findViews();
-        switch (spLoc.getSelectedItem().toString()) {
-            case "食":
-                int intspLoc = 3;
-                break;
-            case "衣":
-                intspLoc = 1;
-                break;
-            case "住":
-                intspLoc = 2;
-                break;
-        }
+
+        spDlvWay=(Spinner) findViewById(R.id.sp_dlvWay);
+        final  String[] dlv={"面交","寄送","面交寄送皆可"};
+        ArrayAdapter<String> dlvList = new ArrayAdapter<>(GoodsInsertActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, dlv);
+        spDlvWay.setAdapter(dlvList);
+        findViews();
+
+
         final Button backButton = (Button)findViewById(R.id.bt_BackGBList);
         final Button dateButton = (Button)findViewById(R.id.bt_date);
         dateButton.setOnClickListener(new View.OnClickListener(){
@@ -201,6 +182,12 @@ public class GoodsInsertActivity extends AppCompatActivity {
             }
         }
     }
+    private void switchFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.body, fragment);
+        fragmentTransaction.commit();
+    }
     public void onNewGoodsResult(View view) {
         //物品名稱驗證
         String name = etName.getText().toString().trim();
@@ -210,7 +197,6 @@ public class GoodsInsertActivity extends AppCompatActivity {
             return;
         }
         //數量驗證
-        String qty=etQty.getText().toString().trim();
         if (etQty.getText().toString().trim().isEmpty()) {
             Toast.makeText(this, R.string.msg_QtyIsInvalid,
                     Toast.LENGTH_SHORT).show();
@@ -240,12 +226,64 @@ public class GoodsInsertActivity extends AppCompatActivity {
     }
 
 
+
         if (Common.networkConnected(this)) {
             int qtyParseInt=Integer.parseInt(etQty.getText().toString().trim());
-            String comment=etComment.toString().trim();
+            String comment=etComment.getText().toString().trim();
+            String goodName=etName.getText().toString().trim();
+
+            //判斷依照地區給予int值
+            switch (spLoc.getSelectedItem().toString()) {
+                case "北":
+                    intspLoc = 1;
+                    break;
+                case "中":
+                    intspLoc = 2;
+                    break;
+                case "南":
+                    intspLoc = 3;
+                    break;
+                default:
+                    intspLoc=0;
+            }
+
+            //判斷依照物品種類給予int值
+            switch (spClass.getSelectedItem().toString()){
+                case "食":
+                    intspClass=1;
+                    break;
+                case "衣":
+                    intspClass=2;
+                    break;
+                case "住":
+                    intspClass=3;
+                    break;
+                case "行":
+                    intspClass=4;
+                    break;
+                case "育":
+                    intspClass=5;
+                    break;
+                case "樂":
+                    intspClass=6;
+                    break;
+            }
+
+            switch (spDlvWay.getSelectedItem().toString()) {
+                case "面交":
+                    intspDlvWay = 1;
+                    break;
+                case "寄送":
+                    intspDlvWay = 2;
+                    break;
+                case "面交寄送皆可":
+                    intspDlvWay = 3;
+                    break;
+            }
+
             String url = Common.URL + "GoodsServlet";
-            Goods goods = new Goods(1, 1, now, "kitty","goodName" ,1,
-                    qtyParseInt, 1, comment, 2, 1995);
+            Goods goods = new Goods(1, 1, now, "kitty",goodName ,intspClass,
+                    qtyParseInt, intspLoc, comment, intspDlvWay, 1995);
             String imageBase64 = Base64.encodeToString(image, Base64.DEFAULT);
             String action = "goodsInsert";
             int count = 0;
@@ -262,11 +300,8 @@ public class GoodsInsertActivity extends AppCompatActivity {
         } else {
             Common.showToast(this, R.string.msg_NoNetwork);
         }
-        finish();
-    }
+       finish();
 
-    public void onCancelClick(View view) {
-        finish();
     }
 }
 
